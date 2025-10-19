3eac20ea360935ecef46b4a878b214bba379c9cb
Barnabas Matrai <matrai.barnabas1@gmail.com>
2025-10-19 13:16:22 +0200

    remove commented out code from Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,9 @@
 import restserver.server.Server;
 
 import java.io.IOException;
-import java.net.InetSocketAddress;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-//        IUserRepository repository = UserRepository.getInstance();
-//        IUserService userService = UserService.getInstance(repository);
-//
-//        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
-//        server.createContext("/users", new UserHandler(userService));
-//        server.setExecutor(null);
         new Server().start();
     }
 }
