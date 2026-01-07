3fcd465aa3788986d03610e6db361b2ed068b8fa
Barnabas Matrai <matrai.barnabas1@gmail.com>
2026-01-07 17:08:08 +0100

    update AuthenticationService

--- src/main/java/service/AuthenticationService.java
+++ src/main/java/service/AuthenticationService.java
@@ -1,12 +1,19 @@
 package service;
 
+import com.sun.net.httpserver.HttpExchange;
+
 import java.security.SecureRandom;
 import java.util.Base64;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class AuthenticationService {
     private static AuthenticationService instance;
     private final SecureRandom random;
     private final Base64.Encoder encoder;
+
+    private Map<String, String> usernamesByTokens = new HashMap<>();
 
     private AuthenticationService() {
         this.random = new SecureRandom();
@@ -21,9 +28,24 @@ public class AuthenticationService {
         return instance;
     }
 
-    public String generateToken() {
+    public String generateToken(String username) {
         byte[] randomBytes = new byte[24];
         random.nextBytes(randomBytes);
-        return encoder.encodeToString(randomBytes);
+        String token = encoder.encodeToString(randomBytes);
+        usernamesByTokens.put(token, username);
+        return token;
     };
+
+    public String getCurrentUser(HttpExchange exchange) {
+        List<String> authHeaders = exchange.getRequestHeaders().get("Authorization");
+        if (authHeaders != null && !authHeaders.isEmpty()) {
+            String authHeader = authHeaders.get(0);
+            String prefix = "Bearer ";
+            if (authHeader.startsWith(prefix)) {
+                String token = authHeader.substring(prefix.length());
+                return usernamesByTokens.get(token);
+            }
+        }
+        return null;
+    }
 }
